f01bb5e8cf8c70133e137a6585eed9d011edb845
Leticia Florêncio <70420362+Leeflorencio@users.noreply.github.com>
2022-11-15 14:37:45 -0300

    Projeto Work out
    Rascunho (de uma parte) do projeto Work out.

---
+++ workout/cadastro.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro</title>
+</head>
+<body>
+    <section>
+        <div>
+            <h3>Cadastre-se</h3>
+
+            <form>
+                <div class="form-group row">
+                    <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+                    <div class="col-sm-10">
+                    <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="inputPassword3" class="col-sm-2 col-form-label">Senha</label>
+                    <div class="col-sm-10">
+                    <input type="password" class="form-control" id="inputPassword3" placeholder="Senha">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </section>
+</body>
+</html>
---
+++ workout/index.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" crossorigin="anonymous">
+
+    <title>Work out</title>
+</head>
+
+<body>
+    <div class="corpo">
+     <header class="menu">
+        <h1>WORK OUT</h1>
+         <nav>
+            <a href="">Quem Somos</a>
+            <a href="#recursos">Recursos</a>
+            <a href="">Criar conta</a>
+            <a href="">Login</a>
+        </nav>
+    </header>
+
+    <section class="chamada">
+        <div class="secao-inicial">
+            <h2 class="text-uppercase"> Controle suas despesas de forma prática e segura </h2>
+            <h3 class="text-muted">Fácil e gratuito!</h3>
+
+            <form class="form-inline">
+                <div class="form-group mb-2">
+                    <label for="staticEmail2" class="sr-only">Email</label>
+                    <input type="text" readonly class="form-control-plaintext border-info" id="staticEmail2" value="email@exemplo.com">
+                </div>
+                <button type="submit" class="btn btn-primary mb-2 bg-info text-white rounded-left teste">CADASTRAR</button>
+            </form>
+
+            <p class="pr-inicial">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum perferendis similique incidunt <br> cumque possimus vero fuga facilis magni nemo asperiores, voluptas aut sapiente <br>voluptate et deleniti, velit, corrupti numquam dolore.
+            </p>
+        </div>
+
+            <div class="img-chamada">
+            <img src="imagens/phone_money.png" alt="Imagem de celular e dinheiro">
+            </div>
+          
+    </section>
+
+    <section id="recursos" class="secao-dois">
+ 
+     <p id="p-dois" class="text-uppercase blockquote text-center">Nossos principais recursos</p>
+     <p class="lead blockquote text-center">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+    
+        <div class="card-deck">
+            <div class="card">
+            <img class="card-img-top" src="imagens/despesas.jpg" alt="Imagem de capa do card">
+            <div class="card-body">
+            <h5 class="card-title">Controle suas despesas</h5>
+            <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum perferendis similique incidunt cumque possimus vero fuga facilis magni nemo consectetur adipisicing elit. </p>
+            </div>
+        </div>
+        <div class="card">
+            <img class="card-img-top" src="imagens/metas.jpg" alt="Imagem de capa do card">
+            <div class="card-body">
+            <h5 class="card-title">Alcance suas metas</h5>
+            <p class="card-text">Eum perferendis similique incidunt cumque possimus vero fuga facilis magni nemo consectetur adipisicing elit.</p>
+            </div>
+        </div>
+        <div class="card">
+            <img class="card-img-top" src="imagens/banco.jpg" alt="Imagem de capa do card">
+            <div class="card-body">
+            <h5 class="card-title">Conciliação bancária</h5>
+            <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum perferendis similique incidunt cumque possimus vero fuga facilis magni nemo consectetur adipisicing elit.</p>
+            </div>
+        </div>
+        <div class="card">
+            <img class="card-img-top" src="imagens/limite.jpg" alt="Imagem de capa do card">
+            <div class="card-body">
+            <h5 class="card-title">Limite de gastos</h5>
+            <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum perferendis similique incidunt cumque possimus vero fuga facilis magni nemo consectetur adipisicing elit. Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            </div>
+         </div>
+    </div>
+    </section>
+
+    <article class="artigo">
+
+        <p id="p-dois" class="text-uppercase blockquote text-center">Nossos principais recursos</p>
+        <p class="lead blockquote text-center">Lorem ipsum, dolor sit amet consectetur adipisicing elit.  Eum perferendis similique incidunt cumque possimus vero fuga.</p>
+        <p class="lead blockquote text-center">Lorem ipsum, dolor sit amet consectetur adipisicing elit vero fuga.</p>
+
+    </article>
+
+    <article class="lista">
+       <!-- <p id="p-dois" class="text-uppercase blockquote text-center">Nossos principais recursos</p> -->
+
+        <div>
+            <img src="imagens/plantar.png" alt="">
+        </div>
+
+        <div class="alertas">
+            <div class="alert alert-light" role="alert">
+                Um simples alerta light. Olha só!
+            </div>
+            <div class="alert alert-light" role="alert">
+                Um simples alerta light. Olha só!
+            </div>
+            <div class="alert alert-light" role="alert">
+                Um simples alerta light. Olha só!
+            </div>
+            <div class="alert alert-light" role="alert">
+                Um simples alerta light. Olha só!
+            </div>
+            <div class="alert alert-light" role="alert">
+                Um simples alerta light. Olha só!
+            </div>
+        </div>
+    </article>
+
+    <footer class="border-top rodape">
+
+            <div class="text-rodape">
+                <h4>WORK OUT</h4>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. <br> Temporibus adipisci  
+                tempore consequatur quia omnis, <br> alias id iste libero, tempora iure.
+                </p>
+            </div>
+
+            <div>
+            <nav class="menu-rodape">
+                    <a class="a-rodape" href="">Quem Somos</a>
+                    <a class="a-rodape" href="">Recursos</a>
+                    <a class="a-rodape" href="">Criar conta</a>
+                    <a class="a-rodape" href="">Login</a>
+                </nav>
+            </div>
+
+            <div class="redes-rodape">
+                    <p class="p-redes">Acompanhe nossas redes sociais</p>
+
+                    <a class="a-redes" href="https://www.facebook.com/" target="blank"><img src="imagens/facebook.png" alt=""></a>
+                    <a class="a-redes" href="https://www.instagram.com/" target="blank"><img src="imagens/instagram.png" alt=""></a>
+                    <a class="a-redes" href="https://twitter.com/home" target="blank"><img src="imagens/twitter.png" alt=""></a>
+                    <a class="a-redes" href="https://www.youtube.com/" target="blank"><img src="imagens/youtube.png" alt=""></a>
+            </div>
+
+            <div class="text-rodape">
+                <p> Projeto desenvolvido para fins educacionais</p>
+                <p>Leticia Florêncio dos Anjos</p>
+                <p>	&copy; 2022</p>
+            </div>
+     
+    </footer>
+
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"  crossorigin="anonymous"></script>
+</body>
+</html>
